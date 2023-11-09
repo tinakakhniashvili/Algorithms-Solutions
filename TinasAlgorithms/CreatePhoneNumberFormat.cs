317354cd26e044da0437777351520bbf7a1f033b
tinakakhniashvili <hosktina@gmail.com>
2023-11-09 08:40:38 -0800

    Add a solution for CreatePhoneNumberFormat.cs

---
+++ TinasAlgorithms/CreatePhoneNumberFormat.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace TinasAlgorithms
+{
+    internal class CreatePhoneNumberFormat
+    {
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (int number in numbers)
+            {
+                sb.Append(number);
+            }
+            sb.Insert(0, '(');
+            sb.Insert(4, ") ");
+            sb.Insert(9, '-');
+
+            return sb.ToString();
+        }
+    }
+}
