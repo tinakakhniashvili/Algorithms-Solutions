ec35960ca3e8fe9e86f11b2e3bc4542f988b173d
tinakakhniashvili <hosktina@gmail.com>
2023-11-11 11:05:04 -0800

    Add CaesarCipherDecrypt method

--- TinasAlgorithms/CaesarCipher.cs
+++ TinasAlgorithms/CaesarCipher.cs
@@ -35,5 +35,36 @@ namespace TinasAlgorithms
 
             return sb.ToString();
         }
+        public static string CaesarCipherDecrypt(string s, int k)
+        {
+            char l;
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            StringBuilder sb = new StringBuilder(s);
+
+            k = k % alphabet.Length;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                bool isUpperCase = Char.IsUpper(s[i]) ? true : false;
+
+                if (Char.IsLetter(s[i]))
+                {
+                    l = Char.ToLower(s[i]);
+
+                    int index = (alphabet.IndexOf(l) - k) % alphabet.Length;
+
+                    if (index < 0) index += alphabet.Length;
+
+                    l = alphabet[index];
+
+                    if (isUpperCase) l = Char.ToUpper(l);
+
+                    sb[i] = l;
+                }
+            }
+
+            return sb.ToString();
+
+        }
     }
 }
