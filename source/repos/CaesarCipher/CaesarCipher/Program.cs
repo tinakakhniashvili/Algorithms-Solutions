36529b309b0a99a7aad6e9ae56b39b66e6be69b4
tinakakhniashvili <hosktina@gmail.com>
2023-11-11 09:03:10 -0800

    Add CaesarCipher.cs

---
+++ source/repos/CaesarCipher/CaesarCipher/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace CaesarCipher
+{
+    internal class Program
+    {
+        public static string caesarCipher(string s, int k)
+        {
+            char l;
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            StringBuilder sb = new StringBuilder(s);
+
+            k = k % alphabet.Length;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                bool isUpperCase = Char.IsUpper(s[i]) ? true : false;
+
+                if (Char.IsLetter(s[i]))
+                {
+                    l = Char.ToLower(s[i]);
+
+                    int index = (alphabet.IndexOf(l) + k) % alphabet.Length;
+
+                    if (index < 0) index += alphabet.Length;
+
+                    l = alphabet[index];
+
+                    if (isUpperCase) l = Char.ToUpper(l);
+
+                    sb[i] = l;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
