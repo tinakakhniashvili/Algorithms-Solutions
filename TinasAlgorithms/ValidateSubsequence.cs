c81e0d5b224f5779112cb724aad1b23fe99094ab
tinakakhniashvili <hosktina@gmail.com>
2023-11-12 08:25:35 -0800

    Add ValidateSubsequence.cs

---
+++ TinasAlgorithms/ValidateSubsequence.cs
@@ -0,0 +1,23 @@
+﻿namespace TinasAlgorithms
+{
+    internal class ValidateSubsequence
+    {
+        public static bool IsValidSubsequence(List<int> array, List<int> sequence)
+        {
+            int i = 0, j = 0;
+            bool isSub = false;
+
+            while (i < array.Count && j < sequence.Count)
+            {
+                if (array[i] == sequence[j]) j++;
+
+                i++;
+            }
+
+            if (j == sequence.Count) isSub = true;
+
+            return isSub;
+
+        }
+    }
+}
