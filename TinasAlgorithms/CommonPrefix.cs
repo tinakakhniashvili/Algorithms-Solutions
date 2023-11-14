932d2ce63f0510e2872f865404934c5bd652817e
tinakakhniashvili <hosktina@gmail.com>
2023-11-14 11:50:54 -0800

    Add UniqueTrueChecker.cs

--- TinasAlgorithms/CommonPrefix.cs
+++ TinasAlgorithms/CommonPrefix.cs
@@ -8,5 +8,25 @@ namespace TinasAlgorithms
 {
     internal class CommonPrefix
     {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            int prefixLength = 0;
+            string prefix = "";
+
+            foreach (char c in strs[0])
+            {
+                foreach (string str in strs)
+                {
+                    if (prefixLength >= str.Length || str[prefixLength] != c)
+                    {
+                        return prefix;
+                    }
+                }
+                prefix += c;
+                prefixLength++;
+            }
+
+            return prefix;
+        }
     }
 }
