3a0c147a276026f7243e137350888241bea25de3
tinakakhniashvili <hosktina@gmail.com>
2023-11-16 09:59:27 -0800

    Add ExtractFirstKWords.cs

---
+++ TinasAlgorithms/ExtractFirstKWords.cs
@@ -0,0 +1,19 @@
+﻿namespace TinasAlgorithms
+{
+    internal class ExtractFirstKWords
+    {
+        public string TruncateSentence(string s, int k)
+        {
+            string[] words = s.Split(' ');
+            string extractedWords = "";
+            for (int i = 0; i < k; i++)
+            {
+                if (i != (k - 1))
+                    extractedWords += words[i] + " ";
+                else
+                    extractedWords += words[i];
+            }
+            return extractedWords;
+        }
+    }
+}
