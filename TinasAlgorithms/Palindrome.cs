68f0fa70bba8ee03f558cf645307ad560623a022
tinakakhniashvili <hosktina@gmail.com>
2023-11-18 11:55:08 -0800

    Add Palindrome.cs

--- TinasAlgorithms/Palindrome.cs
+++ TinasAlgorithms/Palindrome.cs
@@ -8,5 +8,20 @@ namespace TinasAlgorithms
 {
     internal class Palindrome
     {
+        public static bool IsPalindrome(string s)
+        {
+            string[] words = s.Split(' ', ',', '.', ';', ':', '!', '?', '\'', '"', '@', '#', '_', '[', ']', '{', '}', '\\', '-', '(', ')', '`');
+            string word = "";
+            foreach (string w in words) word += w;
+
+            string second = "";
+
+            for (int i = word.Length - 1; i >= 0; i--) second += word[i];
+
+            string firtsPart = word.ToLower();
+            string secondPart = second.ToLower();
+
+            return firtsPart == secondPart;
+        }
     }
 }
