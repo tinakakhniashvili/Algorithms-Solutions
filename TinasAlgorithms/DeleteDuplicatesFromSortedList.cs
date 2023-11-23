35e88eb9d22667d97cba618c77788beb7ffafb2b
tinakakhniashvili <hosktina@gmail.com>
2023-11-23 10:53:55 -0800

    Add DeleteDuplicatesFromSortedList.cs

---
+++ TinasAlgorithms/DeleteDuplicatesFromSortedList.cs
@@ -0,0 +1,21 @@
+﻿namespace TinasAlgorithms
+{
+    internal class DeleteDuplicatesFromSortedList
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode listNode = head;
+
+            while (listNode.next != null)
+            {
+                if (listNode.val == listNode.next.val)
+                    listNode.next = listNode.next.next;
+                else
+                    listNode = listNode.next;
+            }
+
+            return head;
+        }
+    }
+    
+}
--- TinasAlgorithms/LinkedListPalindromeChecker.cs
+++ TinasAlgorithms/LinkedListPalindromeChecker.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TinasAlgorithms
+﻿namespace TinasAlgorithms
 {
     internal class LinkedListPalindromeChecker
     {
+        public bool IsPalindrome(ListNode head)
+        {
+            bool isPalindrome = true;
+            List<int> list = new List<int>();
+
+            while (head != null)
+            {
+                list.Add(head.val);
+                head = head.next;
+            }
+
+            for (int i = 0; i < list.Count / 2; i++)
+            {
+                if (list[i] != list[list.Count - 1 - i])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            return isPalindrome;
+        }
     }
+   /* public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    } */
 }
