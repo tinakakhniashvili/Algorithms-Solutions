c8f78ba396abace3c36d9c97ed0ce8c2d7cb92eb
tinakakhniashvili <hosktina@gmail.com>
2023-11-24 10:54:18 -0800

    Add ReverseLinkedList.cs

---
+++ TinasAlgorithms/ReverseLinkedList.cs
@@ -0,0 +1,27 @@
+﻿namespace TinasAlgorithms
+{
+    internal class ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            List<int> list = new List<int>();
+
+            while (head != null)
+            {
+                list.Add(head.val);
+                head = head.next;
+            }
+
+            ListNode newHead = null;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                ListNode newNode = new ListNode(list[i]);
+                newNode.next = newHead;
+                newHead = newNode;
+            }
+
+            return newHead;
+        }
+    }
+}
