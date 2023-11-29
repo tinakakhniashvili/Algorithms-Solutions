9da18c8110cab949c1815b211ab50fe440fcf759
tinakakhniashvili <hosktina@gmail.com>
2023-11-29 10:44:07 -0800

    Add NodeInsertionInLinkedList.cs

---
+++ TinasAlgorithms/NodeInsertionInLinkedList.cs
@@ -0,0 +1,34 @@
+﻿namespace TinasAlgorithms
+{
+    internal class NodeInsertionInLinkedList
+    {
+        public static SinglyLinkedListNode InsertNodeAtPosition(SinglyLinkedListNode list, int data, int position)
+        {
+            SinglyLinkedListNode newList = new SinglyLinkedListNode();
+            newList.data = data;
+
+            if (position == 0)
+            {
+                newList.next = list;
+                return newList;
+            }
+
+            SinglyLinkedListNode current = list;
+
+            for (int i = 0; i < position - 1; i++)
+                current = current.next;
+
+            if (current == null) return list;
+
+            newList.next = current.next;
+            current.next = newList;
+
+            return list;
+        }
+    }
+    public class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+    }
+}
