47f56d9653283824d6bfc814ab6b9063172a3544
tinakakhniashvili <hosktina@gmail.com>
2023-12-02 08:44:59 -0800

    Add a solution for HighAndLowNumberExtractor.cs

---
+++ TinasAlgorithms/HighAndLowNumberExtractor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TinasAlgorithms
+{
+    internal class HighAndLowNumberExtractor
+    {
+        public static string HighAndLow(string numbers)
+        {
+            List<int> numsList = new List<int>();
+            string[] nums = numbers.Split(' ');
+            foreach (string s in nums)
+            {
+                numsList.Add(int.Parse(s));
+            }
+
+            numsList.Sort();
+
+            return $"{numsList[numsList.Count - 1]} {numsList[0]}";
+        }
+    }
+}
