0658bdb5f6cd47dba70efc85577e0d7a2cad4706
tinakakhniashvili <hosktina@gmail.com>
2023-12-03 13:56:31 -0800

    Add RemainingStudents.cs

---
+++ TinasAlgorithms/RemainingStudents.cs
@@ -0,0 +1,28 @@
+﻿namespace TinasAlgorithms
+{
+    internal class RemainingStudents
+    {
+        public static int CountStudents(List<int> students, List<int> sandwiches)
+        {
+            int count = 0;
+            int count2 = students.Count * 2;
+            while (count != count2)
+            {
+                if (students[0] == sandwiches[0])
+                {
+                    sandwiches.RemoveAt(0);
+                    students.RemoveAt(0);
+                }
+                else
+                {
+                    students.Add(students[0]);
+                    students.RemoveAt(0);
+                }
+
+                count++;
+            }
+
+            return students.Count;
+        }
+    }
+}
