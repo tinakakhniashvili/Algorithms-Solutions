dec6e5044aacabcceea88ac5e80e30093df92012
tinakakhniashvili <hosktina@gmail.com>
2023-12-05 01:44:45 -0800

    Add BinarySearch.cs

---
+++ TinasAlgorithms/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿namespace TinasAlgorithms
+{
+    internal class BinarySearch
+    {
+        public int Search(int[] nums, int target)
+        {
+
+            int left = 0;
+            int right = nums.Length - 1;
+            int middle = (left + right) / 2;
+
+            while (nums[middle] != target)
+            {
+                if (target > nums[middle])
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+
+                if (left > right)
+                {
+                    return -1;
+                }
+
+                middle = (left + right) / 2;
+            }
+
+            return middle;
+        }
+    }
+}
