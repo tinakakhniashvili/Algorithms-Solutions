543ee4bfc634b53f66b5eeeecbcdc190dda923ba
tinakakhniashvili <hosktina@gmail.com>
2023-12-07 11:13:59 -0800

    Add ArrayIntersectionSolution.cs

---
+++ TinasAlgorithms/ArrayIntersectionSolution.cs
@@ -0,0 +1,28 @@
+﻿namespace TinasAlgorithms
+{
+    internal class ArrayIntersectionSolution
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    if (list.Count == nums2.Length)
+                    {
+                        return list.ToArray();
+                    }
+                    else if (nums1[i] == nums2[j])
+                    {
+                        list.Add(nums2[j]);
+                        break; 
+                    }
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
