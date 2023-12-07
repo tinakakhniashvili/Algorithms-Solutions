18817422bf67e0f9a95b4c69888c9f22baa6d48f
tinakakhniashvili <hosktina@gmail.com>
2023-12-07 10:52:22 -0800

    Add FindIntersectionAndDistinct.cs

---
+++ TinasAlgorithms/FindIntersectionAndDistinct.cs
@@ -0,0 +1,22 @@
+﻿namespace TinasAlgorithms
+{
+    internal class FindIntersectionAndDistinct
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    if (nums1[i] == nums2[j])
+                    {
+                        list.Add(nums1[i]);
+                    }
+                }
+            }
+
+            return list.Distinct().ToArray();
+        }
+    }
+}
