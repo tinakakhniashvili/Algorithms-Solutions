940ca5d7e810577e2dd38002e7e4496e9a1d79e6
tinakakhniashvili <hosktina@gmail.com>
2023-12-09 11:58:17 -0800

    Add a solution for IsAnagramCheck.cs

---
+++ TinasAlgorithms/IsAnagramCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinasAlgorithms
+{
+    internal class IsAnagramCheck
+    {
+        public bool IsAnagram(string s, string t)
+        {
+
+            if (s.Length != t.Length) return false;
+
+            List<char> chars = new List<char>();
+
+            foreach (char c in s) chars.Add(c);
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (chars.Contains(t[i])) chars.Remove(t[i]);
+                else return false;
+            }
+
+            return true;
+        }
+    }
+}
