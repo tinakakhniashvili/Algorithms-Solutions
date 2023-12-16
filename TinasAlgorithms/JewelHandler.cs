f8dc25d7f69dd4af02a2fbab062c2fa8eec49370
tinakakhniashvili <hosktina@gmail.com>
2023-12-16 03:17:41 -0800

    Add JewelHandler.cs

---
+++ TinasAlgorithms/JewelHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinasAlgorithms
+{
+    internal class JewelHandler
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            Hashtable jewelTable = new Hashtable();
+
+            foreach (char jewel in jewels)
+            {
+                if (!jewelTable.ContainsKey(jewel))
+                {
+                    jewelTable.Add(jewel, 0);
+                }
+            }
+
+            int count = 0;
+
+            foreach (char stone in stones)
+            {
+                if (jewelTable.ContainsKey(stone))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
