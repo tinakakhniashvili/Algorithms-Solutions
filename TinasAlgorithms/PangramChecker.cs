9f8c475d9078148d49bdc76c3d860a8c30ab85f2
tinakakhniashvili <hosktina@gmail.com>
2023-12-31 03:34:01 -0800

    Add PangramChecker.cs

---
+++ TinasAlgorithms/PangramChecker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace TinasAlgorithms
+{
+    internal class PangramChecker
+    {
+        public bool CheckIfPangram(string sentence)
+        {
+            if (sentence.Length < 26) return false;
+
+            Hashtable letterTable = new Hashtable();
+
+            foreach (char letter in sentence.ToLower())
+            {
+                if (Char.IsLetter(letter))
+                {
+                    if (!letterTable.ContainsKey(letter))
+                    {
+                        letterTable.Add(letter, null);
+                    }
+                }
+            }
+
+            return letterTable.Count == 26;
+        }
+    }
+}
